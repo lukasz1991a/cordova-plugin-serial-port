8fd40aca81da21d92606a10bf51842ccab883a00
LukaszKucza <lukasz.kucza@optimalit.pl>
2023-07-06 10:40:45 +0200

    poprawka

--- www/SerialPortPlugin.js
+++ www/SerialPortPlugin.js
@@ -1,37 +1,37 @@
-cordova.define("com.plugin.SerialPortPlugin.SerialPortPlugin", function (require, exports, module) {
-    let exec = require('cordova/exec');
-    module.exports = class {
-        constructor() {
-            exec((a) => this.activeConnectionNumber = (a - 1), null, "SerialPortPlugin", "create", []);
-        }
+var exec = require('cordova/exec');
 
-        openDevice(settings, success, error) {
-            let waiting = setInterval(() => {
-                if (!Number.isInteger(this.activeConnectionNumber)) return;
-                clearInterval(waiting)
-                exec(success, error, "SerialPortPlugin", "openDevice", [settings, this.activeConnectionNumber]);
-            }, 300)
-        }
 
-        closeDevice(success, error) {
-            exec(success, error, "SerialPortPlugin", "closeDevice", [this.activeConnectionNumber]);
-        }
+var SerialPort = class {
 
-        read(success, error) {
-            exec(success, error, "SerialPortPlugin", "read", [this.activeConnectionNumber]);
-        }
+    constructor() {
+        exec((a) => this.activeConnectionNumber = (a - 1), null, "SerialPortPlugin", "create", []);
+    }
 
-        write(data, success, error) {
-            exec(success, error, "SerialPortPlugin", "write", [data, this.activeConnectionNumber]);
-        }
+    openDevice(settings, success, error) {
+        let waiting = setInterval(() => {
+            if (!Number.isInteger(this.activeConnectionNumber)) return;
+            clearInterval(waiting)
+            exec(success, error, "SerialPortPlugin", "openDevice", [settings, this.activeConnectionNumber]);
+        }, 300)
+    }
 
-        sendDataAndWaitResponse(data, timeoutMs, success, error) {
-            exec(success, error, "SerialPortPlugin", "sendDataAndWaitResponse", [data, timeoutMs, this.activeConnectionNumber]);
-        }
+    closeDevice(success, error) {
+        exec(success, error, "SerialPortPlugin", "closeDevice", [this.activeConnectionNumber]);
+    }
 
-        setHex(isTrue) {
-            exec(null, null, "SerialPortPlugin", "setHex", [isTrue, this.activeConnectionNumber]);
-        }
-    };
+    read(success, error) {
+        exec(success, error, "SerialPortPlugin", "read", [this.activeConnectionNumber]);
+    }
 
-});
+    write(data, success, error) {
+        exec(success, error, "SerialPortPlugin", "write", [data, this.activeConnectionNumber]);
+    }
+
+    sendDataAndWaitResponse(data, timeoutMs, success, error) {
+        exec(success, error, "SerialPortPlugin", "sendDataAndWaitResponse", [data, timeoutMs, this.activeConnectionNumber]);
+    }
+
+    setHex(isTrue) {
+        exec(null, null, "SerialPortPlugin", "setHex", [isTrue, this.activeConnectionNumber]);
+    }
+};
